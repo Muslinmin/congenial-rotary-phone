f49bd444785c24ccc7189abb748ea44aeace115c
Muslinmin (Mus) <crusader2adventureabjure@gmail.com>
2024-09-24 08:18:31 +0800

    Update Nav_Lab.ino @ReubenLow
    Hypotenuse used as distance. XAxis and ZAxis for panning and tilting. Yaxis to represent distance

--- Nav_Lab.ino
+++ Nav_Lab.ino
@@ -1,27 +1,38 @@
-#include <stdio.h>
-
 #include <Servo.h>
 
-#define trig 20
 #define echo 21
+#define trig 20
 
-float t=0, d=0, scale=1e-4; //add scaling calculation here
-char charVal[10];
-String stringVal = " cm";
-Servo myServo;  // Create a Servo object
+#define echo_2 19
+#define trig_2 18
+
+//Y axis - distance measured
+//X axis - yaw or panning
+//Z axis - tilt (UP OR DOWN)
+
+// Linear eqn derived from excel, y=0.9713x+0.0442
+// Polynomial eqn derived from excel, y=0.0006x^2+0.9093x+1.3387
+
+float t=0, distanceX=0, distanceZ=0,hypotenuse=0, scale=1e-4; //add scaling calculation here
+// Eqn gradient m and constant c
+float m = 0.9713, c = 0.0442;
+
+Servo myServo;  // Panning servo X axis
+Servo zServo;
 
 void setup() {
-  // put your setup code here, to run once:
-  // Setup baud ratep
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  //second ultrasonic sensor for Z axis
+  pinMode(trig_2, OUTPUT);
+  pinMode(echo_2, INPUT);
+
   Serial.begin(9600);
-  // Configure GPIO
-  pinMode(trig,OUTPUT);
-  pinMode(echo,INPUT);
-  myServo.attach(9);
-  delay(50);
+  myServo.attach(9);// X axis panning
+  zServo.attach(6); // z axis panning
 }
-void readSensor(){
-    // put your main code here, to run repeatedly:
+void readSensor(int xAxisDeg, int zAxisDeg){
   //Generate trig pulse to start
   digitalWrite(trig,LOW);
   delayMicroseconds(2);
@@ -30,22 +41,46 @@ void readSensor(){
   digitalWrite(trig,LOW);
   //measure TOF and compute distance
   t = pulseIn(echo, HIGH);
-  d = scale* t * 340 / 2;
-  // Use serial print to display result on serial monitor
-  Serial.print("Distance: ");
-  Serial.print(d);
-  Serial.println("cm");
-  delay(50);
+  distanceX = scale* t * 343 / 2;
+
+
+  digitalWrite(trig_2,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_2,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_2,LOW);
+  //measure TOF and compute distance
+  t = pulseIn(echo_2, HIGH);
+  distanceZ = scale* t * 343 / 2;
+
+  hypotenuse = sqrt((distanceX*distanceX) + (distanceZ*distanceZ)); //^2 doesnt work in this case...
+
+  Serial.print(xAxisDeg); 
+  Serial.print(","); 
+  Serial.print(zAxisDeg);
+  Serial.print(","); 
+  Serial.print(hypotenuse);
+  Serial.print(":");
+  //e.g. 90,45,139: "90 degrees from left, 45 degrees from ground, 139cm away"
 }
 void loop() {
-    for (int pos = 0; pos <= 180; pos += 1) {  // Move from 0 to 180 degrees
-    myServo.write(pos);  // Tell servo to go to position in variable 'pos'
-    delay(30);  // Wait 15 milliseconds for the servo to reach the position
-    readSensor();
+    for (int pos = 15; pos <= 180; pos += 1) {
+    myServo.write(pos);
+    delay(15);  
+      for(int zPos = 15; zPos <= 180; zPos+=1){
+        zServo.write(pos);  
+        delay(15); 
+        readSensor(pos, zPos);
+      }
   }
-  for (int pos = 180; pos >= 0; pos -= 1) {  // Move from 180 to 0 degrees
-    myServo.write(pos);  // Tell servo to go to position in variable 'pos'
-    delay(30);  // Wait 15 milliseconds for the servo to reach the position
-    readSensor();
+    for (int pos = 180; pos >= 15; pos -= 1) {
+    myServo.write(pos);
+    delay(15);  
+      for(int zPos = 180; zPos >= 15; zPos-=1){
+        zServo.write(pos);  
+        delay(15); 
+        readSensor(pos, zPos);
+      }
   }
+
 }
