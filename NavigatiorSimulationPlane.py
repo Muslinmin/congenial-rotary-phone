b629a3e22a1832d87a05a3ac73ca8fabf4da87d9
Muslinmin (Mus) <crusader2adventureabjure@gmail.com>
2024-10-05 21:48:17 +0800

    Included plane model

--- NavigatiorSimulationPlane.py
+++ NavigatiorSimulationPlane.py
@@ -5,6 +5,7 @@ from OpenGL.GLU import *
 import pygame
 from pygame.locals import *
 import serial
+from objloader import OBJ  # Import the OBJ loader
 
 # ser = serial.Serial('/dev/tty.usbserial', 38400, timeout=1)
 ser = serial.Serial('COM5', 9600, timeout=1)
@@ -12,6 +13,8 @@ ser = serial.Serial('COM5', 9600, timeout=1)
 ax = ay = az = 0.0
 yaw_mode = False
 
+# Load the OBJ model (global variable)
+model = None
 
 def resize(width, height):
     if height == 0:
@@ -19,19 +22,22 @@ def resize(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(45, 1.0 * width / height, 0.1, 100.0)
+    gluPerspective(45, 1.0 * width / height, 0.1, 700.0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
-
 def init():
+    global model
     glShadeModel(GL_SMOOTH)
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glClearDepth(1.0)
     glEnable(GL_DEPTH_TEST)
     glDepthFunc(GL_LEQUAL)
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
-
+    
+    # Load the OBJ model
+    model = OBJ('./model/plane.obj')  # Adjust the path to your model
+    model.generate()
 
 def drawText(position, textString):
     font = pygame.font.SysFont("Courier", 18, True)
@@ -40,14 +46,13 @@ def drawText(position, textString):
     glRasterPos3d(*position)
     glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
 
-
 def draw():
     global rquad
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glLoadIdentity()
-    glTranslatef(0, 0.0, -7.0)
-
+    glTranslatef(25, -100.0, -450.0)
+    glRotatef(180, 0, 1, 0) 
     osd_text = "pitch: " + str("{0:.2f}".format(ay)) + ", roll: " + str("{0:.2f}".format(ax))
 
     if yaw_mode:
@@ -57,62 +62,23 @@ def draw():
 
     drawText((-2, -2, 2), osd_line)
 
-    # the way I'm holding the IMU board, X and Y axis are switched
-    # with respect to the OpenGL coordinate system
-    if yaw_mode:  # experimental
-        glRotatef(az, 0.0, 1.0, 0.0)  # Yaw,   rotate around y-axis
-    else:
-        glRotatef(0.0, 0.0, 1.0, 0.0)
+    # Apply IMU rotations
+    if yaw_mode:
+        glRotatef(az, 0.0, 1.0, 0.0)  # Yaw, rotate around y-axis
     glRotatef(ay, 1.0, 0.0, 0.0)  # Pitch, rotate around x-axis
-    glRotatef(-1 * ax, 0.0, 0.0, 1.0)  # Roll,  rotate around z-axis
+    glRotatef(-1 * ax, 0.0, 0.0, 1.0)  # Roll, rotate around z-axis
 
-    glBegin(GL_QUADS)
-    glColor3f(0.0, 1.0, 0.0)
-    glVertex3f(1.0, 0.2, -1.0)
-    glVertex3f(-1.0, 0.2, -1.0)
-    glVertex3f(-1.0, 0.2, 1.0)
-    glVertex3f(1.0, 0.2, 1.0)
-
-    glColor3f(1.0, 0.5, 0.0)
-    glVertex3f(1.0, -0.2, 1.0)
-    glVertex3f(-1.0, -0.2, 1.0)
-    glVertex3f(-1.0, -0.2, -1.0)
-    glVertex3f(1.0, -0.2, -1.0)
-
-    glColor3f(1.0, 0.0, 0.0)
-    glVertex3f(1.0, 0.2, 1.0)
-    glVertex3f(-1.0, 0.2, 1.0)
-    glVertex3f(-1.0, -0.2, 1.0)
-    glVertex3f(1.0, -0.2, 1.0)
-
-    glColor3f(1.0, 1.0, 0.0)
-    glVertex3f(1.0, -0.2, -1.0)
-    glVertex3f(-1.0, -0.2, -1.0)
-    glVertex3f(-1.0, 0.2, -1.0)
-    glVertex3f(1.0, 0.2, -1.0)
-
-    glColor3f(0.0, 0.0, 1.0)
-    glVertex3f(-1.0, 0.2, 1.0)
-    glVertex3f(-1.0, 0.2, -1.0)
-    glVertex3f(-1.0, -0.2, -1.0)
-    glVertex3f(-1.0, -0.2, 1.0)
-
-    glColor3f(1.0, 0.0, 1.0)
-    glVertex3f(1.0, 0.2, -1.0)
-    glVertex3f(1.0, 0.2, 1.0)
-    glVertex3f(1.0, -0.2, 1.0)
-    glVertex3f(1.0, -0.2, -1.0)
-    glEnd()
-
+    # Draw the loaded model
+    if model:
+        model.render()  # Call the render method of the OBJ loader
 
 def read_data():
     global ax, ay, az
     ax = ay = az = 0.0
     line_done = 0
 
-    # request data by sending a dot
-    ser.write(b".")  # * encode string to bytes
-    # while not line_done:
+    # Request data by sending a dot
+    ser.write(b".")  # Encode string to bytes
     line = ser.readline()
     angles = line.split(b", ")
     if len(angles) == 3:
@@ -120,7 +86,6 @@ def read_data():
         ay = float(angles[1])
         az = float(angles[2])
         line_done = 1
-
 
 def main():
     global yaw_mode
@@ -134,10 +99,10 @@ def main():
     init()
     frames = 0
     ticks = pygame.time.get_ticks()
-    while 1:
+    while True:
         event = pygame.event.poll()
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pygame.quit()  # * quit pygame properly
+            pygame.quit()  # Quit pygame properly
             break
         if event.type == KEYDOWN and event.key == K_z:
             yaw_mode = not yaw_mode
@@ -146,10 +111,10 @@ def main():
         draw()
 
         pygame.display.flip()
-        frames = frames + 1
+        frames += 1
 
-    print("fps:  %d" % ((frames * 1000) / (pygame.time.get_ticks() - ticks)))
+    print("fps: %d" % ((frames * 1000) / (pygame.time.get_ticks() - ticks)))
     ser.close()
 
-
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
