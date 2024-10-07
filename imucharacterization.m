2ea13cca9feb57210136395377186e4622454fde
Muslinmin (Mus) <crusader2adventureabjure@gmail.com>
2024-10-07 10:49:36 +0800

    Update imucharacterization.m @ReubenLow

--- imucharacterization.m
+++ imucharacterization.m
@@ -1,48 +1,64 @@
-function data = getData(board)
-    msgSize = 1;
-    PACKETS = 11;  % Number of packets to store, 1 packet = 1 samples  
-    datastring = '';
-    dataIn = [];
+%% Sample Routine - DOES NOT WORK/ A SCAM
+% Create the serial object (board)
+board = serialport("COM5", 9600);  % Adjust the COM port and baud rate
 
-    i = 1;
-    for j = 1:PACKETS       
-        % Wait for message Startheader '111'
-        headerCount = 0;
-        while (headerCount < 3)
-            data = read(board, 1, 'char');
-            if (data == '1')
-                headerCount = headerCount + 1;
-            else
-                headerCount = 0;
-            end
-        end
+% Call the getData function
+data = getData(board);
 
-        % Decode message till message endHeader 'FFF' detected
-        headerCount = 0;
-        while (headerCount < 3)
-            data = read(board, 1, 'char');  % Correct usage of 'read'
-            if (data == 'F')
-                headerCount = headerCount + 1;
-            else
-                headerCount = 0;
-                if (data ~= ',')
-                    datastring = strcat(datastring, char(data));
-                else
-                    if (~isempty(datastring))
-                        dataIn(i) = str2double(datastring);
-                        i = i + 1;
-                        datastring = '';
-                    end
-                end
-            end
-        end
+% Update the stripchart for data
+figure(1); 
+StripChart('Update',data(1:end)); title('Channel 1'); drawnow;
+title('Channel 1');
+drawnow;
 
-        % Store the parsed data
-        data = dataIn;
-        disp(data);
-        disp(datastring);
+%% With the Quiver to visualize the orientation of IMU
+% Create the serial object (board)
+board = serialport("COM5", 9600);  % Adjust the COM port and baud rate
 
-        % Flush the serial buffer
-        flush(board);
-    end
+% Set up the figure for the 3D plot
+figure;
+axis([-1 1 -1 1 -1 1]);
+grid on;
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+hold on;
+
+% Initialize quiver objects for X, Y, Z axes
+hx = quiver3(0, 0, 0, 1, 0, 0, 'r', 'LineWidth', 2); % X-axis (red)
+hy = quiver3(0, 0, 0, 0, 1, 0, 'g', 'LineWidth', 2); % Y-axis (green)
+hz = quiver3(0, 0, 0, 0, 0, 1, 'b', 'LineWidth', 2); % Z-axis (blue)
+
+% Loop to update arrows based on IMU data
+while true
+    % Get data from the IMU (pitch, roll, yaw)
+    imuData = getData(board);  % Modify getData to return pitch, roll, yaw
+    
+    % Assuming getData returns angles in degrees: [pitch, roll, yaw]
+    pitch = imuData(7);
+    roll = imuData(8);
+    yaw = 0; % Yaw is fixed as per your requirement
+    
+    % Convert pitch and roll to radians
+    pitch = deg2rad(pitch);
+    roll = deg2rad(roll);
+    
+    % Create rotation matrices for pitch and roll
+    R_pitch = [1, 0, 0; 0, cos(pitch), -sin(pitch); 0, sin(pitch), cos(pitch)];
+    R_roll = [cos(roll), 0, sin(roll); 0, 1, 0; -sin(roll), 0, cos(roll)];
+    
+    % Apply rotations to the base vectors
+    Rx = R_roll * R_pitch * [1; 0; 0];
+    Ry = R_roll * R_pitch * [0; 1; 0];
+    Rz = R_roll * R_pitch * [0; 0; 1];
+    
+    % Update quiver objects to show the new orientation
+    set(hx, 'UData', Rx(1), 'VData', Rx(2), 'WData', Rx(3));
+    set(hy, 'UData', Ry(1), 'VData', Ry(2), 'WData', Ry(3));
+    set(hz, 'UData', Rz(1), 'VData', Rz(2), 'WData', Rz(3));
+    
+    drawnow;
 end
+
+% Flush the serial buffer before starting
+flush(board);
